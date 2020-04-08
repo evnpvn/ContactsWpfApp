7cdc4bb045d8c86c7ee6ac9e7521a1157dd5c78c
evan pavan <evan@evanpavan.com>
2020-04-08 18:05:33 -0400

    Added all project files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contacts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           ReadDatabase();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<ContactPersons>();
+                List<ContactPersons> contacts = connection.Table<ContactPersons>().ToList();
+
+                ContactsGrid.ItemsSource = contacts;
+            }
+        }
+    }
+}
